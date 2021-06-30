ac1a355e360e18e9e3697832b3c2d12102b6b7e2
Marco Pereira <marcopereira@gmx.ch>
2021-06-30 09:59:00 +0000

    corrected docker db host

--- openimis-dist_dkr/docker/compose.sh
+++ openimis-dist_dkr/docker/compose.sh
@@ -7,7 +7,10 @@ export NEW_OPENIMIS_HOST=openimis-hapi.swisstph-mis.ch
 export LEGACY_OPENIMIS_HOST=frontend
 export DB_SQL_SCRIPT=https://github.com/openimis/database_ms_sqlserver/blob/master/Demo%20database/openIMIS_demo_ONLINE.sql?raw=true
 export ACCEPT_EULA=Y
-export DB_HOST=database
+export DB_HOST=openimis_db
+export BE_BRANCH=develop
+export FE_BRANCH=develop
+export GW_BRANCH=develop
 if [ "$1" == "init" ]; then
     # execute other setup scripts or commands here
     # ...
